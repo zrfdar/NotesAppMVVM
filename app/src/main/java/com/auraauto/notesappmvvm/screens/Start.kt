7eb8dae7b1db5e25bec04e4b6d2b123b0ea2eb97
tavrikanec <tavrikanec@mail.ru>
2022-04-23 13:40:41 +0500

    lesson 1

---
+++ app/src/main/java/com/auraauto/notesappmvvm/screens/Start.kt
@@ -0,0 +1,10 @@
+package com.auraauto.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Notes App Screen")
+}
