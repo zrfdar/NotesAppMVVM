fda84ca7530c0e61db539f68634471164c0173cd
tavrikanec <tavrikanec@mail.ru>
2022-04-24 21:34:42 +0500

    Lesson-3

--- app/src/main/java/com/auraauto/notesappmvvm/screens/Main.kt
+++ app/src/main/java/com/auraauto/notesappmvvm/screens/Main.kt
@@ -1,28 +1,41 @@
 package com.auraauto.notesappmvvm.screens
 
+import android.app.Application
 import android.icu.text.CaseMap
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.auraauto.notesappmvvm.MainViewModel
+import com.auraauto.notesappmvvm.MainViewModelFactory
+import com.auraauto.notesappmvvm.model.Note
 import com.auraauto.notesappmvvm.navigation.NavRoute
 import com.auraauto.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
 @Composable
 fun MainScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val mViewModel: MainViewModel = viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+    val notes = mViewModel.readTest.observeAsState(listOf()).value
+
     Scaffold(floatingActionButton = {
         FloatingActionButton(
             onClick = { navController.navigate(NavRoute.Add.route) }
@@ -31,17 +44,20 @@ fun MainScreen(navController: NavHostController) {
         }
     }
     ) {
-        Column() {
+        /*Column() {
             NoteItem(title = "Note 1", subtitle = "Subtitle for note 1", navController = navController)
             NoteItem(title = "Note 2", subtitle = "Subtitle for note 2", navController = navController)
             NoteItem(title = "Note 3", subtitle = "Subtitle for note 3", navController = navController)
             NoteItem(title = "Note 4", subtitle = "Subtitle for note 4", navController = navController)
+        }*/
+        LazyColumn{
+            items(notes) { note -> NoteItem(note = note, navController = navController)}
         }
     }
 }
 
 @Composable
-fun NoteItem(title: String, subtitle: String, navController: NavHostController){
+fun NoteItem(note: Note, navController: NavHostController){
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(vertical = 8.dp, horizontal = 24.dp)
@@ -51,11 +67,11 @@ fun NoteItem(title: String, subtitle: String, navController: NavHostController){
         Column(modifier = Modifier.padding(vertical = 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-                text = title,
+                text = note.title,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
-            Text(text = subtitle)
+            Text(text = note.subtitle)
         }
     }
 }
--- app/src/main/java/com/auraauto/notesappmvvm/screens/Start.kt
+++ app/src/main/java/com/auraauto/notesappmvvm/screens/Start.kt
@@ -1,5 +1,6 @@
 package com.auraauto.notesappmvvm.screens
 
+import android.app.Application
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -7,15 +8,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.auraauto.notesappmvvm.MainViewModel
+import com.auraauto.notesappmvvm.MainViewModelFactory
 import com.auraauto.notesappmvvm.navigation.NavRoute
 import com.auraauto.notesappmvvm.ui.theme.NotesAppMVVMTheme
+import com.auraauto.notesappmvvm.utils.TYPE_FIREBASE
+import com.auraauto.notesappmvvm.utils.TYPE_ROOM
 
 @Composable
 fun StartScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val mViewModel: MainViewModel = viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -27,7 +37,8 @@ fun StartScreen(navController: NavHostController) {
             Text(text = "What we will use?")
             Button(
                 onClick = {
-                          navController.navigate(route = NavRoute.Main.route)
+                    mViewModel.initDatabase(TYPE_ROOM)
+                    navController.navigate(route = NavRoute.Main.route)
                 },
                 modifier = Modifier
                     .width(200.dp)
@@ -37,6 +48,7 @@ fun StartScreen(navController: NavHostController) {
             }
             Button(
                 onClick = {
+                    mViewModel.initDatabase(TYPE_FIREBASE)
                     navController.navigate(route = NavRoute.Main.route)
                 },
                 modifier = Modifier
